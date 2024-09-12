1f7def6e0dc7f53cf030a7a3df502ca8606f9529
SrijanTrivedi00 <147228498+SrijanTrivedi00@users.noreply.github.com>
2024-09-12 13:36:50 +0530

    commit--

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './budget-planner/login/login.component';
+
+export const routes: Routes = [{path:'',component:LoginComponent},
+    {path:'budget-planner',loadChildren:()=> import('./budget-planner/budget-planner.module').then(m=> m.BudgetPlannerModule)}
+];
